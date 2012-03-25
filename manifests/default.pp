2404482e40cba2cf248bbf1837a445c89362ac3e
Bradley Holt <bradley.holt@foundline.com>
2012-03-25 19:39:34 -0400

    Install various packages through Puppet.

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,6 +2,49 @@ class minimal-centos-60 {
   group { "puppet":
     ensure => "present",
   }
+
+  package { "httpd":
+    ensure => latest,
+  }
+
+  package { "php":
+    require => Package["httpd"],
+    ensure => latest,
+    notify => Service["httpd"],
+  }
+
+  package { "php-pecl-apc":
+    require => Package["php"],
+    ensure => latest,
+    notify => Service["httpd"],
+  }
+
+  package { "php-pecl-memcache":
+    require => Package["php"],
+    ensure => latest,
+    notify => Service["httpd"],
+  }
+
+  package { "php-pear":
+    require => Package["php"],
+    ensure => latest,
+    notify => Service["httpd"],
+  }
+
+  package { "php-xml":
+    require => Package["php"],
+    ensure => latest,
+    notify => Service["httpd"],
+  }
+
+  package { "curl":
+    ensure => latest,
+  }
+
+  service { "httpd":
+    ensure => running,
+    require => Package["httpd"]
+  }
 }
 
 include minimal-centos-60
