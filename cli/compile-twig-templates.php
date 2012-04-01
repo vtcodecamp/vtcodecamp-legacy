c4c3157a3cc12db8e03c8aba68a92b463c84c2d8
Bradley Holt <bradley.holt@foundline.com>
2012-04-01 15:21:02 -0400

    Output details of template compiling.

--- cli/compile-twig-templates.php
+++ cli/compile-twig-templates.php
@@ -6,7 +6,12 @@ require APPLICATION_ROOT . '/vendor/.composer/autoload.php';
 
 $twigConfig = include APPLICATION_ROOT . '/config/twig.php';
 
+foreach ($twigConfig['paths'] as $path) {
+    echo 'Path: ' . $path . PHP_EOL;
+}
 $loader = new Twig_Loader_Filesystem($twigConfig['paths']);
+echo 'Cache: '. $twigConfig['environment']['cache'] . PHP_EOL;
+echo 'Debug: '. $twigConfig['environment']['debug'] . PHP_EOL;
 $twig = new Twig_Environment($loader, $twigConfig['environment']);
 
 foreach ($twigConfig['templates'] as $templateName) {
