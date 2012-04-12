288b1688f8122e3cd9b13c16eb679fc501d0fe94
Bradley Holt <bradley.holt@foundline.com>
2012-04-12 18:11:58 -0400

    Create and test time period value object.

---
+++ src/VtCodeCamp/TimePeriod.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace VtCodeCamp;
+
+use \DateTime;
+
+/**
+ * @category    VtCodeCamp
+ * @package     VtCodeCamp_TimePeriod
+ */
+class TimePeriod
+{
+    /**
+     * @var \DateTime
+     */
+    private $start;
+
+    /**
+     * @var DateTime
+     */
+    private $end;
+
+    public function __construct(DateTime $start, DateTime $end)
+    {
+        $this->start = clone $start;
+        $this->end = clone $end;
+    }
+
+    /**
+     * Get Start
+     * 
+     * @return \DateTime
+     */
+    public function getStart()
+    {
+        return clone $this->start;
+    }
+
+    /**
+     * Get End
+     * 
+     * @return \DateTime
+     */
+    public function getEnd()
+    {
+        return clone $this->end;
+    }
+}
