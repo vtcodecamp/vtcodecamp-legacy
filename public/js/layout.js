914c29223e28c5a8e9a3db54fbb1bc0a855caec2
Jason Pelletier <jason.pelletier@foundline.com>
2012-04-27 20:07:30 -0400

    Update name of data attribute used to replace SVG backgrounds with SVG elements.

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("[data-ui-replace=true]").each(function() {
+    $("[data-svg-replace=true]").each(function() {
         var src = $(this).css("backgroundImage").match(/^url\("(.*)"\)$/)[1];
         $(this).svg({
             loadURL: src,
