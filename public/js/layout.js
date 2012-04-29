85e3e16f679fc0e6142419590ae7cd510c2c738e
Jason Pelletier <jason.pelletier@foundline.com>
2012-04-29 08:52:57 -0400

    Add smooth scrolling for anchor links.

--- public/js/layout.js
+++ public/js/layout.js
@@ -68,4 +68,22 @@ $(document).ready(function() {
             "opacity": "0"
         }, 200); 
     });
+    var scrolling = 0;
+    $(".anchor").bind("scrollTo", function(e) {
+        var scrollTarget = "#" + $(this).attr("id");
+        scrolling++;
+        $.smoothScroll({
+            scrollTarget: scrollTarget,
+            afterScroll: function () {
+                if (1 == scrolling) {
+                    location.hash = scrollTarget;
+                }
+                scrolling--;
+            }
+        });
+    });
+    $("a[data-anchor]").click(function() {
+        $($(this).attr("href")).trigger("scrollTo");
+        return false;
+    });
 });
